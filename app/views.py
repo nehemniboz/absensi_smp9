32660e86e397bf26725d7f1cea07727215c0c133
nehemia artah sasta <nehemiaals@gmail.com>
2024-05-04 01:15:10 +0700

    bebas3

--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def dashboard(request):
-    context={}
+    context={
+        'nama': 'arie'
+    }
     return render(request, 'dashboard.html', context)
 
--- core/settings.py
+++ core/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app', 
+    'account',
     'crispy_forms', 
     'crispy_bootstrap4', 
+
 ]
 
 MIDDLEWARE = [
@@ -134,3 +136,11 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+LOGIN_REDIRECT_URL = 'dashboard'
+
+LOGOUT_REDIRECT_URL = 'login'
